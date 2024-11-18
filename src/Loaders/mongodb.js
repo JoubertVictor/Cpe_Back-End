273ca74ca8e52051701bbd20f138724b1729a97a
JoubertVictor <johnvict323@gmail.com>
2024-11-17 21:56:52 -0300

    o

---
+++ src/Loaders/mongodb.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+async function startDB() {
+    await mongoose.connect('mongodb+srv://joubertsharma:OVSPF7oNtWEjnjE7@cpeback.jyj06.mongodb.net/?retryWrites=true&w=majority&appName=CpeBack')
+}
+
+module.exports = startDB;
