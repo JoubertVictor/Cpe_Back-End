5c35fb27f3271fba1ded6d0027d81c0acc8701b2
JoubertVictor <johnvict323@gmail.com>
2024-11-18 22:48:35 -0300

    Melhorando rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const rotas = require("./routes")
+const rotas = require("./routes");
+const cors =requeire("cors");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
+app.use("*", (req, res) => {
+    res.status(404).json({message: `Rota '${req.baseUrl}' nao encnotrada`})
+})
 
 module.exports = app;
