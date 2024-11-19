bd64c8924d54ea45d7b02cef350c7990d98040bd
JoubertVictor <johnvict323@gmail.com>
2024-11-18 22:31:34 -0300

    CRUD

---
+++ src/Controllers/SessoeController.js
@@ -0,0 +1,31 @@
+const SessoesModel = require("../Models/SessoesModel");
+const { populate } = require("../Models/UsuarioModel");
+
+class SessoesController {
+
+    async create(req, res){
+       const sessoes = await SessoesModel.create(req.body);
+
+       return res.status(200).json(sessoes);
+    }
+
+    async read(req, res){
+        const sessoess = await SessoesModel.find().populate('id_usuario', '-senha');
+
+        return res.status(200).json(sessoess);
+    }
+
+    async update(req, res){
+        
+    }
+
+    async delete(req, res){
+         const { id } = req.params
+
+         await SessoesModel.findByIdAndDelete(id);
+
+         return res.status(200).json({"mensagem": "Sessao deletado com sucesso"});
+    }
+}
+
+module.exports = new SessoesController();
--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -8,18 +8,26 @@ class UsuarioController {
        return res.status(200).json(usuario);
     }
 
-    async rea(req, res){
+    async read(req, res){
         const usuarios = await UsuarioModel.find();
 
         return res.status(200).json(usuarios);
     }
 
     async update(req, res){
-        
+        const { id } = req.params;
+
+        const usuario = await UsuarioModel.findByIdAndUpdate(id, req.body,{ new: true });
+
+        return res.status(200).json(usuario);
     }
 
-    delete(req, res){
-        
+    async delete(req, res){
+         const { id } = req.params
+
+         await UsuarioModel.findByIdAndDelete(id);
+
+         return res.status(200).json({"mensagem": "Usuario deletado com sucesso"});
     }
 }
 
