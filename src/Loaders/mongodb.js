4dac1a79c260847b89395dd7401044acf6701876
JoubertVictor <johnvict323@gmail.com>
2024-11-21 03:47:21 -0300

    Aula13

--- src/Loaders/mongodb.js
+++ src/Loaders/mongodb.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 async function startDB() {
-    await mongoose.connect('mongodb+srv://joubertsharma:OVSPF7oNtWEjnjE7@cpeback.jyj06.mongodb.net/?retryWrites=true&w=majority&appName=CpeBack')
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Banco de dados iniciado")
 }
 
 module.exports = startDB;
